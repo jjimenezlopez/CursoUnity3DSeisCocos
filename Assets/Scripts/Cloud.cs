efdb480e40dbfa4b1f2f7f5a01494439465c9f8b
Jose Jiménez <jjimenezlopez@gmail.com>
2015-10-12 21:12:58 +0200

    Very first and basic clouds implementation

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cloud : MonoBehaviour {
+
+	public float speed;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (0, -speed * Time.deltaTime, 0);
+	}
+}
